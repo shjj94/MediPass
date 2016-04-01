1fdfd2f13cf3eca6ff26d785165e31e79aa042f4
Nara Shin <robotluv1226@naver.com>
2016-04-02 00:42:21 +0900

    nfc개발위한 파일 생성

---
+++ MediPassNfc/app/src/main/java/com/medi/medipass/join.java
@@ -0,0 +1,26 @@
+package com.medi.medipass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by shjjthjj on 2016-03-25.
+ */
+public class join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+
+        Button bt_back = (Button)findViewById(R.id.back_join);
+        bt_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+}
---
+++ MediPassNfc/app/src/main/java/com/medi/medipass/myPage_logInfo.java
@@ -0,0 +1,13 @@
+package com.medi.medipass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myPage_logInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_page_log_info);
+    }
+}
