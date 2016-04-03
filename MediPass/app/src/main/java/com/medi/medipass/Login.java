99e10b24d4e18db33dd5f20473d5ea056c99157b
shjj94 <gusthdb94@naver.com>
2016-04-03 09:02:21 +0900

    공통파일변경

--- MediPass/app/src/main/java/com/medi/medipass/Login.java
+++ MediPass/app/src/main/java/com/medi/medipass/Login.java
@@ -1,17 +1,22 @@
 package com.medi.medipass;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class index extends AppCompatActivity {
+public class Login extends AppCompatActivity {
+
+    public static Activity login_activity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        login_activity = Login.this;
 
         Button bt_login = (Button)findViewById(R.id.bt_login);
         Button bt_join = (Button)findViewById(R.id.bt_join);
@@ -19,7 +24,7 @@ public class index extends AppCompatActivity {
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_login = new Intent(getApplicationContext(), main.class);
+                Intent intent_login = new Intent(getApplicationContext(), Home.class);
                 startActivity(intent_login);
             }
         });
@@ -27,7 +32,7 @@ public class index extends AppCompatActivity {
         bt_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_join = new Intent(getApplicationContext(), join.class);
+                Intent intent_join = new Intent(getApplicationContext(), Join.class);
                 startActivity(intent_join);
             }
         });
--- MediPass/app/src/main/java/com/medi/medipass/myPage.java
+++ MediPass/app/src/main/java/com/medi/medipass/myPage.java
@@ -6,18 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class myPage extends AppCompatActivity {
+public class MyPage extends AppCompatActivity {
+
+    Record record_activity = (Record)Record.record_activity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_page);
+
+        if(record_activity != null) {
+            record_activity.finish();
+        }
 
         Button myPage_logInfo = (Button)findViewById(R.id.mypage_info);
         myPage_logInfo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent_mypage_loginfo = new Intent(getApplicationContext(), myPage_logInfo.class);
+                Intent intent_mypage_loginfo = new Intent(getApplicationContext(), MyPageLogInfo.class);
                 startActivity(intent_mypage_loginfo);
             }
         });
