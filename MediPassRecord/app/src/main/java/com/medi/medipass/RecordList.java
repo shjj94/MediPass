1049f3b626b7c929e6494a3d3a7fbf30131b3644
shjj94 <gusthdb94@naver.com>
2016-04-15 01:31:18 +0900

    진료기록-리스트에 custom listview추가 -> 다음단계 : php를 통해 mysql과 연결하여 서버의 진료기록을 리스트에 띄우기

--- MediPassRecord/app/src/main/java/com/medi/medipass/RecordList.java
+++ MediPassRecord/app/src/main/java/com/medi/medipass/RecordList.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+/* listView 잘 설명된 블로그 : http://jo.centis1504.net/?p=1009 */
+/* http://recipes4dev.tistory.com/43 */
 
 public class RecordList extends Fragment {
 
@@ -12,6 +16,31 @@ public class RecordList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.record_list, container, false);
 
-        return view;
+        ListView listview;
+        final ListViewAdapter adapter;
+
+        //Adapter생성
+        adapter = new ListViewAdapter();
+
+        //리스트뷰 참조 및 Adapter달기
+        listview = (ListView)view.findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+
+        //아이템 추가
+        adapter.addItem("2016-01-02", "장염");
+        adapter.addItem("2016-01-03", "배탈");
+        adapter.addItem("2016-01-04", "설사");
+
+        /*
+        //클릭 이벤트 정의
+        listview.setOnClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                //
+            }
+        });
+        */
+
+       return view;
     }
 }
