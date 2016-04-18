d0958917490af2da3bb0800bd47a9239b782f4c1
Nara Shin <robotluv1226@naver.com>
2016-04-18 21:05:01 +0900

    nfc 라이트 앱 & 시연영상

--- MediPass/app/src/main/java/com/medi/medipass/ListViewAdapter.java
+++ MediPass/app/src/main/java/com/medi/medipass/ListViewAdapter.java
@@ -68,6 +68,7 @@ public class ListViewAdapter extends BaseAdapter {
 
     //아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
     public void addItem(String date, String disName){
+
         Log.d("PHP", "addItem");
         ListViewItem item = new ListViewItem();
 
@@ -75,5 +76,6 @@ public class ListViewAdapter extends BaseAdapter {
         item.setItem_disName(disName);
 
         listViewItemList.add(item);
+        notifyDataSetChanged();
     }
 }
